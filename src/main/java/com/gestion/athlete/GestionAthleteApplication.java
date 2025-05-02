51808af8c8f9288f4c5042ce2d77e5a557a8500b
azizftw7 <benzarbaziz.2015@gmail.com>
2025-05-02 22:34:23 +0100

    Initial commit - Gestion Athlete project

---
+++ src/main/java/com/gestion/athlete/GestionAthleteApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.athlete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAthleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAthleteApplication.class, args);
+	}
+
+}
